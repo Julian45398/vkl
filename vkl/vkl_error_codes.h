1868851bec024d657ecb84bd63e39feecd438334
akroZora <schreyerjulian@googlemail.com>
2024-11-08 12:12:54 +0100

    moved error code

--- vkl/vkl_error_codes.h
+++ vkl/vkl_error_codes.h
@@ -2,23 +2,24 @@
 
 
 namespace vkl {
-	const char VKL_ERROR_SWAPCHAIN_CREATION_FAILED[] = "failed to create swapchain";
-	const char VKL_ERROR_INSTANCE_CREATION_FAILED[] = "failed to create instance";
-	const char VKL_ERROR_DEVICE_CREATION_FAILED[] = "failed to create device";
-	const char VKL_ERROR_RENDER_PASS_CREATION_FAILED[] = "failed to create renderpass";
-	const char VKL_ERROR_FRAMEBUFFER_CREATION_FAILED[] = "failed to create framebuffer";
-	const char VKL_ERROR_PIPELINE_CREATION_FAILED[] = "failed to create pipeline";
-	const char VKL_ERROR_PIPELINE_LAYOUT_CREATION_FAILED[] = "failed to create pipeline layout";
-	const char VKL_ERROR_SHADER_MODULE_CREATION_FAILED[] = "failed to create shader module";
-	const char VKL_ERROR_IMAGE_CREATION_FAILED[] = "failed to create image";
-	const char VKL_ERROR_IMAGE_VIEW_CREATION_FAILED[] = "failed to create image view";
-	const char VKL_ERROR_BUFFER_CREATION_FAILED[] = "failed to create buffer";
-	const char VKL_ERROR_MEMORY_ALLOCATION_FAILED[] = "failed to allocate memory";
-	const char VKL_ERROR_FENCE_CREATION_FAILED[] = "failed to create fence";
-	const char VKL_ERROR_SEMAPHORE_CREATION_FAILED[] = "failed to create semaphore";
-	const char VKL_ERROR_COMMANDBUFFER_CREATION_FAILED[] = "failed to create command buffer";
-	const char VKL_ERROR_COMMANDPOOL_CREATION_FAILED[] = "failed to create command pool";
-	const char VKL_ERROR_COMMANDPOOL_RESET_FAILED[] = "failed to reset command pool";
-	const char VKL_ERROR_BUFFER_MEMORY_REQ_ACQUIRED_FAILED[] = "filed to acquire buffer memory requirements";
-	const char VKL_ERROR_IMAGE_MEMORY_REQ_ACQUIRED_FAILED[] = "filed to acquire image memory requirements";
+	inline const char VKL_ERROR_SWAPCHAIN_CREATION_FAILED[] = "failed to create swapchain";
+	inline const char VKL_ERROR_INSTANCE_CREATION_FAILED[] = "failed to create instance";
+	inline const char VKL_ERROR_DEVICE_CREATION_FAILED[] = "failed to create device";
+	inline const char VKL_ERROR_RENDER_PASS_CREATION_FAILED[] = "failed to create renderpass";
+	inline const char VKL_ERROR_FRAMEBUFFER_CREATION_FAILED[] = "failed to create framebuffer";
+	inline const char VKL_ERROR_PIPELINE_CREATION_FAILED[] = "failed to create pipeline";
+	inline const char VKL_ERROR_PIPELINE_LAYOUT_CREATION_FAILED[] = "failed to create pipeline layout";
+	inline const char VKL_ERROR_SHADER_MODULE_CREATION_FAILED[] = "failed to create shader module";
+	inline const char VKL_ERROR_IMAGE_CREATION_FAILED[] = "failed to create image";
+	inline const char VKL_ERROR_IMAGE_VIEW_CREATION_FAILED[] = "failed to create image view";
+	inline const char VKL_ERROR_BUFFER_CREATION_FAILED[] = "failed to create buffer";
+	inline const char VKL_ERROR_MEMORY_ALLOCATION_FAILED[] = "failed to allocate memory";
+	inline const char VKL_ERROR_FENCE_CREATION_FAILED[] = "failed to create fence";
+	inline const char VKL_ERROR_SEMAPHORE_CREATION_FAILED[] = "failed to create semaphore";
+	inline const char VKL_ERROR_COMMANDBUFFER_CREATION_FAILED[] = "failed to create command buffer";
+	inline const char VKL_ERROR_COMMANDPOOL_CREATION_FAILED[] = "failed to create command pool";
+	inline const char VKL_ERROR_COMMANDPOOL_RESET_FAILED[] = "failed to reset command pool";
+	inline const char VKL_ERROR_BUFFER_MEMORY_REQ_ACQUIRED_FAILED[] = "filed to acquire buffer memory requirements";
+	inline const char VKL_ERROR_IMAGE_MEMORY_REQ_ACQUIRED_FAILED[] = "filed to acquire image memory requirements";
+	inline const char VKL_ERROR_MEMORY_MAPPING_FAILED[] = "failed to map memory!";
 }
