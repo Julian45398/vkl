54fcc3c791b10ad3c82417cbc256a7e0aa842593
akroZora <schreyerjulian@googlemail.com>
2024-11-08 23:43:27 +0100

    added synchronization functions

--- vkl/synchronization.h
+++ vkl/synchronization.h
@@ -12,6 +12,15 @@ namespace vkl {
 		VKL_CHECK(vkCreateFence(device, &info, VKL_Callbacks, &fence), VKL_ERROR_FENCE_CREATION_FAILED);
 		return fence;
 	}
+	inline void waitForFence(VkDevice device, VkFence fence, uint64_t timeout = UINT64_MAX) {
+		VKL_CHECK(vkWaitForFences(device, 1, &fence, VK_TRUE, timeout), VKL_ERROR_FENCE_WAIT_FAILED);
+	}
+	inline void waitForFences(VkDevice device, uint32_t fenceCount, const VkFence* pFences, VkBool32 waitAll = VK_TRUE, uint64_t timeout = UINT64_MAX) {
+		VKL_CHECK(vkWaitForFences(device, fenceCount, pFences, waitAll, timeout), VKL_ERROR_FENCE_WAIT_FAILED);
+	}
+	inline void destroyFence(VkDevice device, VkFence fence) {
+		VKL_CHECK(vkDestroyFence(device, fence, VKL_Callbacks), VKL_ERROR_FENCE_DESTRUCTION_FAILED);
+	}
 	inline VkSemaphore createSemaphore(VkDevice device, VkSemaphoreCreateFlags flags = VKL_FLAG_NONE, const void* pNext = nullptr) {
 		VkSemaphore semaphore;
 		VkSemaphoreCreateInfo info {
@@ -20,5 +29,8 @@ namespace vkl {
 		};
 		VKL_CHECK(vkCreateSemaphore(device, &info, VKL_Callbacks, &semaphore), VKL_ERROR_SEMAPHORE_CREATION_FAILED);
 		return semaphore;
+	}
+	inline void destroySemaphore(VkDevice device, VkSemaphore semaphore) {
+		VKL_CHECK(vkDestroySemaphore(device, semaphore, VKL_Callbacks), VKL_ERROR_SEMAPHORE_DESTRUCTION_FAILED);
 	}
 }
